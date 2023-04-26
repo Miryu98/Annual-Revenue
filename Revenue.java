a4bddb59a571e4776db00865cff4ec7b74823596
Miłosz Salwa <75585351+Miryu98@users.noreply.github.com>
2023-04-26 18:08:46 +0200

    Update Revenue.java

--- Revenue.java
+++ Revenue.java
@@ -1,7 +1,8 @@
 public class Revenue {
     public static void main(String[] args) {
+        
         double[] revenue = {88750, 125430, 99700, 14500, 158000, 65000, 99000, 189000, 210000, 42000, 165800, 258900};
-        //your code goes here
+        
         double sum = 0.0;
 
         for (int i = 0; i < revenue.length; i++) {
